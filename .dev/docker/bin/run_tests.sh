e9b1f6a29fd73053dfbaa9b51628bd46da69f982
Dmytro Demchyna <dmitry.demchina@gmail.com>
2019-05-06 12:56:12 +0300

    Codecov support (#4)

--- .dev/docker/bin/run_tests.sh
+++ .dev/docker/bin/run_tests.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker-compose run --rm php /data/vendor/bin/phpunit -c /data/tests/phpunit.xml /data/tests/SchemaKeeper
+docker-compose run --rm php /data/vendor/bin/phpunit -c /data/tests/phpunit.xml --coverage-clover=/data/coverage.xml /data/tests/SchemaKeeper
